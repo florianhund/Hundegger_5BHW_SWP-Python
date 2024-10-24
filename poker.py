63957a19a3af07cdd9bbb884c7ed4b6fe6d92265
Florian Hundegger <flo.hundegger@gmail.com>
2024-10-24 09:06:46 +0200

    refactor: improve performance by reducing loops

--- poker.py
+++ poker.py
@@ -1,17 +1,13 @@
 import random
 from collections import defaultdict
 
-
 colors = ("Karo", "Herz", "Pik", "Kreuz")
 symbols = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14")
 # 11 = Bube, 12 = Dame, 13 = König, 14 = Ass
 
 
 def generate_deck():
-    deck = [
-        {"symbol": symbol, "color": color} for symbol in symbols for color in colors
-    ]
-    return deck
+    return [{"symbol": symbol, "color": color} for symbol in symbols for color in colors]
 
 
 def draw_hand(deck):
@@ -19,55 +15,47 @@ def draw_hand(deck):
     return deck[:5]
 
 
-def get_symbols(hand):
-    return [card["symbol"] for card in hand]
+def get_hand_details(hand):
+    symbols = [card["symbol"] for card in hand]
+    colors = [card["color"] for card in hand]
+    symbol_counts = {}
+
+    for symbol in symbols:
+        if symbol in symbol_counts:
+            symbol_counts[symbol] += 1
+        else:
+            symbol_counts[symbol] = 1
+
+    return symbols, colors, symbol_counts
 
 
-def get_colors(hand):
-    return [card["color"] for card in hand]
+def has_same_values(symbol_counts, count=2):
+    return any(val == count for val in symbol_counts.values())
 
 
-def has_same_values(hand, count=2):
-    values = get_symbols(hand)
-    for value in values:
-        if values.count(value) == count:
-            return True
-    return False
+def is_two_pair(symbol_counts):
+    return list(symbol_counts.values()).count(2) == 2
 
 
-def is_two_pair(hand):
-    values = get_symbols(hand)
-    pairs = set()
-
-    for value in values:
-        if values.count(value) == 2:
-            pairs.add(value)
-
-    return len(pairs) == 2
+def is_full_house(symbol_counts):
+    return has_same_values(symbol_counts, 2) and has_same_values(symbol_counts, 3)
 
 
-def is_full_house(hand):
-    return has_same_values(hand, 2) and has_same_values(hand, 3)
-
-
-def is_flush(hand):
-    colors = get_colors(hand)
+def is_flush(colors):
     return len(set(colors)) == 1
 
 
-def is_street(hand):
-    values = sorted([int(card["symbol"]) for card in hand])
-    if len(set(values)) != 5:
-        return False
-    return values[-1] - values[0] == 4
+def is_street(symbols):
+    values = sorted([int(symbol) for symbol in symbols])
+    return len(set(values)) == 5 and values[-1] - values[0] == 4
 
 
-def is_straight_flush(hand):
-    return is_flush(hand) and is_street(hand)
+def is_straight_flush(symbols, colors):
+    return is_flush(colors) and is_street(symbols)
 
 
-def is_royal_flush(hand):
-    return is_straight_flush(hand) and "14" in get_symbols(hand)
+def is_royal_flush(symbols, colors):
+    return is_straight_flush(symbols, colors) and "14" in symbols
 
 
 def simulate_games(num_games=100000):
@@ -75,27 +63,26 @@ def simulate_games(num_games=100000):
     counter = defaultdict(int)
 
     for _ in range(num_games):
-        # optimize code to evaluate symbols and colors only once
-        # to prevent for loop in every check
         hand = draw_hand(deck)
+        symbols, colors, symbol_counts = get_hand_details(hand)
 
-        if is_royal_flush(hand):
+        if is_royal_flush(symbols, colors):
             counter["Royal Flush"] += 1
-        elif is_straight_flush(hand):
+        elif is_straight_flush(symbols, colors):
             counter["Straight Flush"] += 1
-        elif has_same_values(hand, 4):
+        elif has_same_values(symbol_counts, 4):
             counter["Vierling"] += 1
-        elif is_full_house(hand):
+        elif is_full_house(symbol_counts):
             counter["Full House"] += 1
-        elif is_flush(hand):
+        elif is_flush(colors):
             counter["Flush"] += 1
-        elif is_street(hand):
+        elif is_street(symbols):
             counter["Straße"] += 1
-        elif is_two_pair(hand):
+        elif is_two_pair(symbol_counts):
             counter["Zwei Paare"] += 1
-        elif has_same_values(hand, 3):
+        elif has_same_values(symbol_counts, 3):
             counter["Drilling"] += 1
-        elif has_same_values(hand, 2):
+        elif has_same_values(symbol_counts, 2):
             counter["Paar"] += 1
 
     return counter
